d1fb2bcc4765155e0e697d83ebb259035cd5c549
unknown <scott.ndjofang@facsciences-uy1.cm>
2026-01-18 20:24:33 +0800

    calculator v0.1

--- calculator.js
+++ calculator.js
@@ -1,12 +1,61 @@
 const buttons= document.querySelectorAll('button');
 const display = document.querySelector('.display');
 
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
 
 buttons.forEach(button => {
 
    button.addEventListener('click',() => {
 
-    display.textContent += button.textContent;
+    var value= button.textContent;
+
+           if (!isNaN(value) || value === '.') {
+                if (operator === '') {
+                    firstNumber += value;
+                    display.textContent = firstNumber;
+                } else {
+                    secondNumber += value;
+                     display.textContent = firstNumber + ' ' + operator + ' ' + secondNumber;
+                }
+            } else if (['+', '-', '*', '/'].includes(value)) {
+            operator = value;
+            display.textContent = firstNumber + ' ' + operator;
+            }
+            else if (value === '=') {
+            let result;
+
+        switch (operator) {
+            case '+':
+            result = Number(firstNumber) + Number(secondNumber);
+            break;
+            case '-':
+            result = Number(firstNumber) - Number(secondNumber);
+            break;
+            case '*':
+            result = Number(firstNumber) * Number(secondNumber);
+            break;
+            case '/':
+            result = Number(firstNumber) / Number(secondNumber);
+            break;
+            default:
+            return;
+        }
+
+         display.textContent = result;
+            firstNumber = result.toString();
+            secondNumber = '';
+            operator = '';
+    } 
+          else if (value === 'C') {
+      firstNumber = '';
+      secondNumber = '';
+      operator = '';
+      display.textContent = '0';
+    } 
 });
 
 }) ;
+
+
